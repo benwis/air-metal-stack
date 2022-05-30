ebf1c67e591c8236ceacabcadb52dd967fe0c2ae
Ben Wishoshavich <ben@celcyon.com>
2022-05-30 09:36:14 -0700

    Fix title

--- packages/remix-app/app/root.tsx
+++ packages/remix-app/app/root.tsx
@@ -11,7 +11,7 @@ import * as React from "react";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Air Metal Stack Demo",
   viewport: "width=device-width,initial-scale=1",
 });
 
