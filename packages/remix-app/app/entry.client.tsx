5da41bee3474f2443fa69d4fec463a7c2240b646
Ben Wishoshavich <ben@celcyon.com>
2022-06-07 11:04:09 -0700

    Updated React to v18.1.0 and changed hydrate to hydrateRoot

--- packages/remix-app/app/entry.client.tsx
+++ packages/remix-app/app/entry.client.tsx
@@ -1,9 +1,9 @@
 import { RemixBrowser } from "@remix-run/react";
 import * as React from "react";
-import { hydrate } from "react-dom";
+import { hydrateRoot } from "react-dom/client";
 import init from "../../rust_functions/build/browser/rust_functions"
 import wasm from "../../rust_functions/build/browser/rust_functions_bg.wasm"
 
 init(wasm).then(() => {
-    hydrate(<RemixBrowser />, document);
+    hydrateRoot(document, <RemixBrowser />);
 })
